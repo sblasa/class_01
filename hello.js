c70742bf883410f1ccadcc8eebf6c1e823bfb62d
sblasa <stblasa@gmail.com>
2017-07-19 20:17:40 -0700

    initial write

---
+++ hello.js
@@ -0,0 +1,11 @@
+var whoToSayYo = "Sussette";
+var myPrintLoops = 10;
+
+
+for(var i = 0; i < myPrintLoops; i++ ) {
+
+    if(i % 2 != 0) {
+    console.log("Print: " + i + " Hello, " + whoToSayYo +"!");
+    }
+
+}
